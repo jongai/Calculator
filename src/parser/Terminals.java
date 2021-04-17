d8f889dfc06dd89b435c83d6f766c45a10ada499
Jonathan Gai <jonathangai2003@gmail.com>
2021-04-17 16:09:02 -0400

    Add parenthesis support

--- src/parser/Terminals.java
+++ src/parser/Terminals.java
@@ -2,14 +2,16 @@ package parser;
 
 /**
  * This class lists terminals used by the
- * grammar specified in "path.grammar".
+ * grammar specified in "calculator.grammar".
  */
 public class Terminals {
-	static public final short DIVIDE = 3;
+	static public final short DIVIDE = 4;
 	static public final short EOF = 0;
-	static public final short INT_LIT = 1;
-	static public final short MINUS = 6;
-	static public final short MOD = 4;
-	static public final short PLUS = 5;
-	static public final short TIMES = 2;
+	static public final short INT_LIT = 2;
+	static public final short LPAREN = 1;
+	static public final short MINUS = 7;
+	static public final short MOD = 5;
+	static public final short PLUS = 6;
+	static public final short RPAREN = 8;
+	static public final short TIMES = 3;
 }
